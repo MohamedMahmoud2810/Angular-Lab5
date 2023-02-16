b4504c4e461332dcdeaf85d7e7993655d729b734
mohamed mahmoud <mohammedmahmmoud05@gmail.com>
2023-02-16 14:24:27 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DetailsComponent } from './details/details.component';
+import { ErrorComponent } from './error/error.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule , Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
+const routes:Routes = [{path:'',component:RegistrationComponent} ,
+                {path:'register' , component:RegistrationComponent},
+                {path:'profile',component:ProfileComponent} ,
+                {path:'details/:id',component:DetailsComponent} ,
+                {path:'**',component:ErrorComponent} ,
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    ProfileComponent,
+    DetailsComponent,
+    ErrorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
